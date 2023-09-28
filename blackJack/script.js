3d0e968ecdd874dea5d5c77d5f7708111f32243b
LamaPirat <g.skrefsrud@gmail.com>
2023-09-28 15:24:03 +0200

    created renderCards() function for rendering everytime a player hit

--- blackJack/script.js
+++ blackJack/script.js
@@ -17,6 +17,9 @@ class Player {
     this.money = money;
     this.cards = [];
   }
+  getCards() {
+    return this.cards;
+  }
 
   addCard(card) {
     this.cards.push(card);
@@ -51,14 +54,37 @@ function createDeck() {
 
 let deckOfCards = createDeck();
 let player = new Player(1000);
+let amountOfCardsHit = 0;
 
 function hitCard() {
   const rNum = Math.floor(Math.random() * 52);
   let card = deckOfCards[rNum];
   deckOfCards.splice(rNum, 1);
   player.addCard(card);
-  console.log(player);
-  console.log(deckOfCards);
+  renderCards();
+  amountOfCardsHit++;
 }
 
 function skipCard() {}
+
+function renderCards() {
+  let playerCardsContainer = document.querySelector(".playerCards");
+  let currentPlayerCards = player.getCards();
+  console.log(currentPlayerCards[amountOfCardsHit].getNumber());
+  let cardDisplay = document.createElement("div");
+  let cardDisplayValue = document.createElement("p");
+  let cardDisplaySort = document.createElement("p");
+
+  cardDisplay.classList.add("cardDisplay");
+  cardDisplayValue.classList.add("cardDisplayValue");
+  cardDisplayValue.textContent = `${currentPlayerCards[
+    amountOfCardsHit
+  ].getNumber()}`;
+  cardDisplaySort.classList.add("classDisplaySort");
+  cardDisplaySort.textContent = `${currentPlayerCards[
+    amountOfCardsHit
+  ].getSort()}`;
+  cardDisplay.appendChild(cardDisplayValue);
+  cardDisplay.appendChild(cardDisplaySort);
+  playerCardsContainer.appendChild(cardDisplay);
+}
