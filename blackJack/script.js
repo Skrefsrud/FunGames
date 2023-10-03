4affc10f0def54e99fa9676c50546ce72e158aa3
LamaPirat <g.skrefsrud@gmail.com>
2023-10-03 16:02:18 +0200

    added some more game logic, won screen, lost screen and money display, also new game button with logic for game reset

--- blackJack/script.js
+++ blackJack/script.js
@@ -34,12 +34,28 @@ class Player {
     this.cards = [];
   }
 
+  getMoney() {
+    return this.money;
+  }
+
   addMoney(sum) {
     this.money += sum;
   }
 
   removeMoney(sum) {
     this.money -= sum;
+  }
+
+  getSum() {
+    let sum = 0;
+    for (let i = 0; i < this.cards.length; i++) {
+      if (this.cards[i].getNumber() < 10) {
+        sum += this.cards[i].getNumber();
+      } else {
+        sum += 10;
+      }
+    }
+    return sum;
   }
 }
 
@@ -61,6 +77,28 @@ function createDeck() {
 let deckOfCards = createDeck();
 let player = new Player(1000);
 let dealer = new Player(1000);
+let betAmount = 250;
+
+//Function for resetting game
+function resetGame() {
+  player.removeCards();
+  dealer.removeCards();
+  deckOfCards = createDeck();
+  document.querySelector(".playerCards").innerHTML = "";
+  document.querySelector(".dealerCards").innerHTML = "";
+  document.getElementById(
+    "player-sum"
+  ).innerHTML = `Player sum: ${player.getSum()}`;
+  document.getElementById(
+    "dealer-sum"
+  ).innerHTML = `Dealer sum: ${dealer.getSum()}`;
+  let lScreen = document.getElementById("lostScreen");
+  if (lScreen === null) {
+    document.getElementById("wonScreen").remove();
+  } else {
+    lScreen.remove();
+  }
+}
 
 //Functions for giving player and dealer a new card and removing it from the deck
 
@@ -70,6 +108,50 @@ function hitPlayerCard() {
   deckOfCards.splice(rNum, 1);
   player.addCard(card);
   renderPlayerCards();
+  if (player.getSum() > 21) {
+    playerLost();
+  }
+}
+
+function playerLost() {
+  dealer.addMoney(betAmount);
+  let div = document.createElement("div");
+  div.setAttribute("id", "lostScreen");
+  let btn = document.createElement("button");
+  btn.innerHTML = "New round";
+  btn.classList.add("newRoundBtn");
+  btn.addEventListener("click", function () {
+    resetGame();
+  });
+  div.appendChild(btn);
+  let h1 = document.createElement("h1");
+  h1.innerHTML = "DEALER WON!";
+  div.appendChild(h1);
+  document.body.appendChild(div);
+}
+
+function playerWon() {
+  dealer.addMoney(betAmount * 2);
+  dealer.removeMoney(betAmount);
+  let div = document.createElement("div");
+  div.setAttribute("id", "wonScreen");
+  let btn = document.createElement("button");
+  btn.innerHTML = "New round";
+  btn.classList.add("newRoundBtn");
+  btn.addEventListener("click", function () {
+    resetGame();
+  });
+  div.appendChild(btn);
+  let h1 = document.createElement("h1");
+  h1.innerHTML = "PLAYER WON!";
+  div.appendChild(h1);
+  document.body.appendChild(div);
+  renderPlayerMoney();
+}
+
+function renderPlayerMoney() {
+  let moneyDisplay = document.getElementById("player-money");
+  moneyDisplay.innerHTML = `${player.getMoney()}$`;
 }
 
 function hitDealerCard() {
@@ -88,7 +170,16 @@ function dealCards() {
   }
 }
 
-function stand() {}
+function stand() {
+  while (dealer.getSum() < player.getSum()) {
+    hitDealerCard();
+  }
+  if (dealer.getSum() > 21) {
+    playerWon();
+  } else {
+    playerLost();
+  }
+}
 
 //functions for rendering player and dealers cards
 
@@ -111,6 +202,10 @@ function renderPlayerCards() {
   cardDisplay.appendChild(cardDisplayValue);
   cardDisplay.appendChild(cardDisplaySort);
   playerCardsContainer.appendChild(cardDisplay);
+
+  document.getElementById(
+    "player-sum"
+  ).innerHTML = `Player sum: ${player.getSum()}`;
 }
 
 function renderDealerCards() {
@@ -132,4 +227,8 @@ function renderDealerCards() {
   cardDisplay.appendChild(cardDisplayValue);
   cardDisplay.appendChild(cardDisplaySort);
   dealerCardsContainer.appendChild(cardDisplay);
+
+  document.getElementById(
+    "dealer-sum"
+  ).innerHTML = `Dealer sum: ${dealer.getSum()}`;
 }
