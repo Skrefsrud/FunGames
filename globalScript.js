83c6b646fdfdfd7e55f6e04b762d7cfad8b9209b
LamaPirat <g.skrefsrud@gmail.com>
2023-10-19 14:58:19 +0200

    fixed animation to begin halfway through, added background animation to dokumenter

---
+++ globalScript.js
@@ -0,0 +1,8 @@
+//Assign background spans random variable number between 1 - 41
+let backgroundBubles = document.querySelector(".background-bubles");
+let bubles = backgroundBubles.querySelectorAll("span");
+bubles.forEach((buble) => {
+  let randomNumber = Math.floor(Math.random() * 41) + 1;
+  buble.style = `--i: ${randomNumber}`;
+  buble.classList.remove("hidden");
+});
